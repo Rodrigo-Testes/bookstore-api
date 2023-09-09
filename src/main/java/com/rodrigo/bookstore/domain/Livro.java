f29aed159d7797d5b210d81e8837a2704cfbe049
Rodrigo-Testes <rodrigo.santos@ciee.ong.br>
2023-09-09 09:50:22 -0300

    classes de dominio iniciada

---
+++ src/main/java/com/rodrigo/bookstore/domain/Livro.java
@@ -0,0 +1,95 @@
+package com.rodrigo.bookstore.domain;
+
+import java.util.Objects;
+
+public class Livro {
+
+	private Long id;
+	private String titulo;
+	private String nome_autor;
+	private String texto;
+
+	// associacao
+	private Categoria categoria;
+
+	// ==============================================================================
+	// constructs
+
+	public Livro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Livro(Long id, String titulo, String nome_autor, String texto, Categoria categoria) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.nome_autor = nome_autor;
+		this.texto = texto;
+		this.categoria = categoria;
+	}
+
+	// ==============================================================================
+	// getters e setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getNome_autor() {
+		return nome_autor;
+	}
+
+	public void setNome_autor(String nome_autor) {
+		this.nome_autor = nome_autor;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+
+	// ==============================================================================
+	// hashcode e equals
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(id, other.id);
+	}
+}
